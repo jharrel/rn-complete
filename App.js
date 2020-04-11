38bbfed87467d76af9d3ec989c4bea4d327643a8
jharrel <harrel.jason@gmail.com>
2020-04-11 15:04:46 -0700

    Added more CSS React guides

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 50}}>
-      <View style={{}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
         <TextInput placeholder="Course Goal" 
-        style={{borderColor: 'black', borderWidth: 1 }} />
+        style={{ width: 200, borderColor: 'black', borderWidth: 1 }} />
         <Button title="ADD" />
       </View>
       <View>
